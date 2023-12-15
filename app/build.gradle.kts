6e4841d159cca5a901f70231f246f0015ee1d116
Rafael Marçal <rafaelmarcal2103@gmail.com>
2023-12-15 15:48:44 -0300

    Autentificação do numero parcialmente feita.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,4 +39,11 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.hbb20:ccp:2.5.0")
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+    implementation ("com.google.firebase:firebase-auth:latest_version")
+
 }
--- app/src/main/java/br/edu/ifsp/dmo/projetofinaldmo/LoginOtpActivity.java
+++ app/src/main/java/br/edu/ifsp/dmo/projetofinaldmo/LoginOtpActivity.java
@@ -1,26 +1,106 @@
 package br.edu.ifsp.dmo.projetofinaldmo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.Firebase;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.ktx.Firebase;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import br.edu.ifsp.dmo.projetofinaldmo.utils.AndroidUtil;
 
 public class LoginOtpActivity extends AppCompatActivity {
 
     String phoneNumber;
+    Long timeoutSeconds = 60L;
+    String verificationCode;
+    PhoneAuthProvider.ForceResendingToken resendingToken;
+
+    EditText otpInput;
+    Button nextBtn;
+    ProgressBar progressBar;
+    TextView resendOtpTextView;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_otp);
 
+        otpInput = findViewById(R.id.login_otp);
+        nextBtn = findViewById(R.id.login_next_btn);
+        progressBar = findViewById(R.id.login_progress_bar);
+        resendOtpTextView = findViewById(R.id.resend_otp_textview);
+
         phoneNumber = getIntent().getExtras().getString("phone");
-        Toast.makeText(getApplicationContext(),phoneNumber,Toast.LENGTH_LONG).show();
+
+        sendOtp(phoneNumber, false);
+    }
+
+    void sendOtp(String phoneNumber,boolean isResend){
+        setInProgress(true);
+        PhoneAuthOptions.Builder builder =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(phoneNumber)
+                        .setTimeout(timeoutSeconds, TimeUnit.SECONDS)
+                        .setActivity(this)
+                        .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                signIn(phoneAuthCredential);
+                                setInProgress(false);
+                            }
+
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+                                AndroidUtil.showToast(getApplicationContext(),"OTP verification failed");
+                                setInProgress(false);
+                            }
+
+                            @Override
+                            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                super.onCodeSent(s, forceResendingToken);
+                                verificationCode = s;
+                                resendingToken = forceResendingToken;
+                                AndroidUtil.showToast(getApplicationContext(),"OTP sent succesfully");
+                                setInProgress(false);
+                            }
+                        });
+        if (isResend){
+            PhoneAuthProvider.verifyPhoneNumber(builder.setForceResendingToken(resendingToken).build());
+        }else {
+            PhoneAuthProvider.verifyPhoneNumber(builder.build());
+        }
+
+    }
+
+    void setInProgress(boolean inProgress){
+        if (inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            nextBtn.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            nextBtn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    void signIn(PhoneAuthCredential phoneAuthCredential){
+        //Login e vai para a proxima activity
     }
 }
