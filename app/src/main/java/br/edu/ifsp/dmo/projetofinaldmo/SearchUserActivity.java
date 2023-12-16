2f9bb746e778d8879f22087a80f015cf1c2fbcd5
Rafael Marçal <rafaelmarcal2103@gmail.com>
2023-12-16 10:50:43 -0300

    Criado o menu de navegação e o buscador de usuários.

--- app/src/main/java/br/edu/ifsp/dmo/projetofinaldmo/SearchUserActivity.java
+++ app/src/main/java/br/edu/ifsp/dmo/projetofinaldmo/SearchUserActivity.java
@@ -1,14 +1,45 @@
 package br.edu.ifsp.dmo.projetofinaldmo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class SearchUserActivity extends AppCompatActivity {
+
+    EditText searchInput;
+    ImageButton searchButton;
+    ImageButton backButton;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_user);
+
+        searchInput = findViewById(R.id.search_username_input);
+        searchButton = findViewById(R.id.search_user_btn);
+        backButton = findViewById(R.id.back_btn);
+        recyclerView = findViewById(R.id.search_user_recycler_view);
+
+        searchInput.requestFocus();
+
+        backButton.setOnClickListener(v ->{
+            onBackPressed();
+        });
+
+        searchButton.setOnClickListener(v -> {
+            String searchTerm = searchInput.getText().toString();
+            if (searchTerm.isEmpty() || searchTerm.length()<3){
+                searchInput.setError("Nome de Usuário Invalido");
+                return;
+            }
+            setUpSearchRecyclerView(searchTerm);
+        });
+    }
+
+    void setUpSearchRecyclerView(String searchTerm) {
     }
 }
