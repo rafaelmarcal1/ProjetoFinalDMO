c977b174f3fbe7f4a5f4372f1cf43333d7c1558e
Rafael Marçal <rafaelmarcal2103@gmail.com>
2023-12-18 14:21:21 -0300

    Usuarios trocando mensagem com sucesso!

--- app/src/main/java/br/edu/ifsp/dmo/projetofinaldmo/ChatActivity.java
+++ app/src/main/java/br/edu/ifsp/dmo/projetofinaldmo/ChatActivity.java
@@ -1,5 +1,6 @@
 package br.edu.ifsp.dmo.projetofinaldmo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -8,12 +9,24 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.util.Arrays;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
+
+import br.edu.ifsp.dmo.projetofinaldmo.model.ChatMessageModel;
+import br.edu.ifsp.dmo.projetofinaldmo.model.ChatroomModel;
 import br.edu.ifsp.dmo.projetofinaldmo.model.UserModel;
 import br.edu.ifsp.dmo.projetofinaldmo.utils.AndroidUtil;
+import br.edu.ifsp.dmo.projetofinaldmo.utils.FirebaseUtil;
 
 public class ChatActivity extends AppCompatActivity {
 
     UserModel otherUser;
+    String chatroomId;
+    ChatroomModel chatroomModel;
 
     EditText messageInput;
     ImageButton sendMessageBtn;
@@ -28,6 +41,7 @@ public class ChatActivity extends AppCompatActivity {
 
         //get UserModel
         otherUser = AndroidUtil.getUserModelFromIntent(getIntent());
+        chatroomId = FirebaseUtil.getChatroomId(FirebaseUtil.currentUserID(),otherUser.getUserId());
 
         messageInput = findViewById(R.id.chat_message_input);
         sendMessageBtn = findViewById(R.id.message_send_btn);
@@ -39,5 +53,50 @@ public class ChatActivity extends AppCompatActivity {
             onBackPressed();
         });
         otherUsername.setText(otherUser.getUsername());
+
+        sendMessageBtn.setOnClickListener((v -> {
+            String message = messageInput.getText().toString().trim();
+            if (message.isEmpty())
+                return;
+            sendMessageToUser(message);
+        }));
+
+        getOrCreateChatroomModel();
+    }
+
+    void sendMessageToUser(String message){
+
+        chatroomModel.setLastMessageTimestamp(Timestamp.now());
+        chatroomModel.setLastMessageSenderId(FirebaseUtil.currentUserID());
+        chatroomModel.setLastMessage(message);
+        FirebaseUtil.getChatroomReference(chatroomId).set(chatroomModel);
+
+        ChatMessageModel chatMessageModel = new ChatMessageModel(message,FirebaseUtil.currentUserID(),Timestamp.now());
+        FirebaseUtil.getChatroomMessageReference(chatroomId).add(chatMessageModel)
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        if(task.isSuccessful()){
+                            messageInput.setText("");
+                        }
+                    }
+                });
+    }
+    void getOrCreateChatroomModel(){
+        FirebaseUtil.getChatroomReference(chatroomId).get().addOnCompleteListener(task -> {
+            if(task.isSuccessful()){
+                chatroomModel = task.getResult().toObject(ChatroomModel.class);
+                if(chatroomModel==null){
+                    //first time chat
+                    chatroomModel = new ChatroomModel(
+                            chatroomId,
+                            Arrays.asList(FirebaseUtil.currentUserID(),otherUser.getUserId()),
+                            Timestamp.now(),
+                            ""
+                    );
+                    FirebaseUtil.getChatroomReference(chatroomId).set(chatroomModel);
+                }
+            }
+        });
     }
 }
